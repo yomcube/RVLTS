0825fca34280d55db1eb15b9526631b52c213b32
TomCube <121310847+yomcube@users.noreply.github.com>
2025-05-01 17:39:13 -0500

    CARDFileInfo

---
+++ RVLTS/revolution/CARD/card.ts
@@ -0,0 +1,11 @@
+// kiwi515/ogws:
+//   https://github.com/kiwi515/ogws/blob/master/include/revolution/CARD/CARD.h
+
+export type CARDFileInfo = {
+    chan: number;
+    fileNo: number;
+    offset: number;
+    length: number;
+    iBlock: number;
+    padding: number;
+};
