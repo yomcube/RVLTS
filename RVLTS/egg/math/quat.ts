61452acb5427872e8b444bf0f75a46495a95f6d7
TomCube <121310847+yomcube@users.noreply.github.com>
2025-05-16 11:46:05 -0500

    egg/math/quat.Quatf

---
+++ RVLTS/egg/math/quat.ts
@@ -0,0 +1,30 @@
+// kiwi515/ogws:
+//   https://github.com/kiwi515/ogws/blob/master/include/egg/math/eggQuat.h
+//   https://github.com/kiwi515/ogws/blob/master/src/egg/math/eggQuat.cpp
+
+import { Vector3f } from "egg/math/vector";
+
+class Quatf {
+    x: number;
+    y: number;
+    z: number;
+    w: number;
+
+    constructor(_w: number, vec: Vector3f) {
+        this.x = vec.x;
+        this.y = vec.y;
+        this.z = vec.z;
+        this.w = _w;
+    }
+
+    setAxisRotation(axis: Vector3f, angle: number) {
+        let angleHalf: number = angle * 0.5;
+        let angleCos: number = Math.cos(angleHalf);
+        let angleSin: number = Math.sin(angleHalf);
+
+        this.w = angleCos;
+        this.z = angleSin * axis.z;
+        this.x = angleSin * axis.x;
+        this.y = angleSin * axis.y;
+    }
+}
