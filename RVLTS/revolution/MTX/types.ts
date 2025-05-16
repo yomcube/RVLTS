270ed9337dc71bfe990e26bdf6089af1ac756dd2
TomCube <121310847+yomcube@users.noreply.github.com>
2025-05-16 13:40:22 -0500

    fix revolution/MTX/types.Mtx

--- RVLTS/revolution/MTX/types.ts
+++ RVLTS/revolution/MTX/types.ts
@@ -1,12 +1,10 @@
 // kiwi515/ogws:
 //   https://github.com/kiwi515/ogws/blob/master/include/revolution/MTX/mtxtypes.h
 
-// This is the only way we can declare fixed-length arrays in TS :(
 export type Mtx = [
-    [number, number, number],
-    [number, number, number],
-    [number, number, number],
-    [number, number, number],
+    [number, number, number, number],
+    [number, number, number, number],
+    [number, number, number, number],
 ];
 export type Mtx44 = [
     [number, number, number, number],
