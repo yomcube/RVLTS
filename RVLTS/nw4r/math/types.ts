db0c435570377080271959202d09e99c94ca58e4
TomCube <121310847+yomcube@users.noreply.github.com>
2025-05-20 11:41:00 -0500

    nw4r/math/types.MTX33

--- RVLTS/nw4r/math/types.ts
+++ RVLTS/nw4r/math/types.ts
@@ -2,6 +2,10 @@
 //   https://github.com/kiwi515/ogws/blob/master/include/nw4r/math/math_types.h
 
 
+//////////////
+//// VEC2 ////
+//////////////
+
 interface _VEC2 {
 	x: number;
 	y: number;
@@ -52,6 +56,11 @@ export class VEC2 implements _VEC2 {
 		return this;
 	}
 }
+
+
+//////////////
+//// VEC3 ////
+//////////////
 
 interface _VEC3 {
 	x: number;
@@ -186,5 +195,74 @@ export class VEC3 implements _VEC3 {
 		out.x = v1.x * x;
 		out.y = v1.y * x;
 		out.z = v1.z * x;
+	}
+}
+
+
+///////////////
+//// MTX33 ////
+///////////////
+
+type _MTX33_a = [
+	number, number, number,
+	number, number, number,
+	number, number, number
+];
+type _MTX33_m = [
+	[number, number, number],
+	[number, number, number],
+	[number, number, number]
+];
+function und0(x: number | undefined): number {
+	return x == undefined ? 0 : x;
+}
+export class MTX33 {
+	_00: number; _01: number; _02: number;
+	_10: number; _11: number; _12: number;
+	_20: number; _21: number; _22: number;
+
+	constructor()
+	constructor(
+		f00: number, f01: number, f02: number,
+		f10: number, f11: number, f12: number,
+		f20: number, f21: number, f22: number
+	)
+	constructor(
+		f00?: number, f01?: number, f02?: number,
+		f10?: number, f11?: number, f12?: number,
+		f20?: number, f21?: number, f22?: number
+	) {
+		this._00 = und0(f00); this._01 = und0(f01); this._02 = und0(f02);
+		this._10 = und0(f10); this._11 = und0(f11); this._12 = und0(f12);
+		this._20 = und0(f20); this._21 = und0(f21); this._22 = und0(f22);
+	}
+
+	// Unions don't work the same in TypeScript and C, so getters
+	// and setters are used to replicate the functionality.
+
+	get a(): _MTX33_a {
+		return [
+			this._00, this._01, this._02,
+			this._10, this._11, this._12,
+			this._20, this._21, this._22
+		];
+	}
+	set a(a: _MTX33_a) {
+		this._00 = a[0]; this._01 = a[1]; this._02 = a[2];
+		this._10 = a[3]; this._11 = a[4]; this._12 = a[5];
+		this._20 = a[6]; this._21 = a[7]; this._22 = a[8];
+	}
+
+	get m(): _MTX33_m {
+		return [
+			[this._00, this._01, this._02],
+			[this._10, this._11, this._12],
+			[this._20, this._21, this._22]
+		];
+	}
+	set m(m: _MTX33_m) {
+		this._00 = m[0][0]; this._01 = m[0][1]; this._02 = m[0][2];
+		this._10 = m[1][0]; this._11 = m[1][1]; this._12 = m[1][2];
+		this._20 = m[2][0]; this._21 = m[2][1]; this._22 = m[2][2];
 	}
 }
