a1ced08b8407e97fbd2c42759acee26f262f934c
TomCube <121310847+yomcube@users.noreply.github.com>
2025-05-22 13:53:38 -0500

    revolution/MTX/mtxvec

---
+++ RVLTS/revolution/MTX/mtxvec.ts
@@ -0,0 +1,20 @@
+// kiwi515/ogws:
+//   https://github.com/kiwi515/ogws/blob/master/include/revolution/MTX/mtxvec.h
+//   https://github.com/kiwi515/ogws/blob/master/src/revolution/MTX/mtxvec.c
+
+import { Mtx, Vec } from "revolution/MTX/types";
+
+export function mtxMultVec(out: Vec, mtx: Mtx, vec: Vec) {
+    out.x = (
+        mtx[0][0] * vec.x + mtx[0][1] * vec.y +
+        mtx[0][2] * vec.z + mtx[0][3]
+    );
+    out.y = (
+        mtx[1][0] * vec.x + mtx[1][1] * vec.y +
+        mtx[1][2] * vec.z + mtx[1][3]
+    );
+    out.z = (
+        mtx[2][0] * vec.x + mtx[2][1] * vec.y +
+        mtx[2][2] * vec.z + mtx[2][3]
+    );
+}
